f71f7eb51d4919f5ac59fc61b2a7fca0a8dea765
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-05 07:38:45 +0100

    simple search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from "react";
+import "./App.css";
+
+import { data } from "./data";
+import { Form, InputGroup, Table } from "react-bootstrap";
 
 function App() {
+
+  const [search, setSearch] =useState('')
+  console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form>
+    <InputGroup>
+    <Form.Control placeholder="search contacts" onChange={(e) => setSearch(e.target.value)} />
+    
+    </InputGroup>
+
+      </Form>
+      <Table>
+        <thread>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thread>
+        <tbody>
+          {data.filter((item) =>{
+            return search.toLowerCase() === '' ? item :item.first_name.toLowerCase().includes(search)
+          }).map(({first_name,last_name,email,phone,id}) => (
+            <tr key={id}>
+              <td>{first_name}</td>
+              <td>{last_name}</td>
+              <td>{email}</td>
+              <td>{phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
